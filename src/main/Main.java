744d83b3916374fd50a43aad84e1927e1f874abf
Érika Mota <erikamota@protonmail.com>
2021-06-26 17:14:15 -0300

    Criação da janela do jogo e inicio da programação dos graficos e da classe player

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,26 +2,70 @@ package main;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+
 import javax.swing.*;
 
-@SuppressWarnings("serial")
-public class Main extends JFrame{
+@SuppressWarnings({ "unused", "serial" })
+public class Main extends Canvas implements Runnable{
 
+	public static final int WIDTH = 800, HEIGTH = 600;
+	
 	public Main() {
-		
-		this.setTitle("Mari0");
-		this.setSize(1024,728);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-		this.setResizable(false);
-		
+		Dimension dimension = new Dimension(WIDTH,HEIGTH);
+		this.setPreferredSize(dimension);		
 	}
 	
 	public static void main(String[] args) {
-		
-		Main window = new Main();
+		Main game = new Main();
+		JFrame window = new JFrame();
+		window.add(game);
+		window.setTitle("Mari0");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();
+		window.setLocationRelativeTo(null);
+		window.setResizable(false);
 		window.setVisible(true);
 		
+		new Thread(game).start();
 	}
 
+	
+	public void update() {
+		System.out.println("lolol");
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		//BACKGROUND
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGTH);
+		//LIFE CONTER		
+		g.setColor(Color.white);
+		g.setFont(new Font("Arial",Font.BOLD,22));
+		g.drawString("LIFES: 3", WIDTH/5, HEIGTH/12);
+		//SPRITES
+		bs.show();
+	}
+	
+	public void run() {
+		while(true) {
+			update();
+			render();
+			try {
+				Thread.sleep(1000/60);
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
 }
---
+++ src/main/Player.java
@@ -0,0 +1,54 @@
+package main;
+
+public class Player {
+	private int x;
+	private int y;
+	private int acel;
+	private int lifes = 3;
+	
+	public void jump() {
+		
+	}
+	
+	public void portalBlue() {
+		
+	}
+	
+	public void portalOrange() {
+		
+	}
+	
+	public void carryBox() {
+		
+	}
+
+	
+////////////GETS E SETS////////////
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getAcel() {
+		return acel;
+	}
+	public void setAcel(int acel) {
+		this.acel = acel;
+	}
+
+	public int getLifes() {
+		return lifes;
+	}
+	public void setLifes(int lifes) {
+		this.lifes = lifes;
+	}
+}
